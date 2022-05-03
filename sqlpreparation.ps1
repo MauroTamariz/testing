6b74c56d4a03038422ec42f4524ef4f111fe8950
mauricio.cardona <mauricio.cardona@fisglobal.com>
2022-05-03 16:22:45 -0500

    Execution sqlsetting.ps1

--- sqlpreparation.ps1
+++ sqlpreparation.ps1
@@ -3,9 +3,10 @@ Param (
     [Parameter(Mandatory=$true)]
     [string] $ServiceAccount
 )
-
-
 (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/MauroTamariz/testing/main/sqlsettings.ps1','C:\sqlsettings.ps1')
+if ($ServiceAccount){
+    (gc C:\sqlsettings.ps1) -replace 'accountToReplace', $ServiceAccount | Out-File -encoding ASCII C:\sqlsettings.ps1
+}
+cd C:\
+./sqlsettings.ps1
 
-#powershell -Command "(gc C:\sqlsettings.ps1) -replace 'accountToReplace', $ServiceAccount | Out-File -encoding ASCII C:\sqlsettings.ps1"
-(gc C:\sqlsettings.ps1) -replace 'accountToReplace', $ServiceAccount | Out-File -encoding ASCII C:\sqlsettings.ps1
