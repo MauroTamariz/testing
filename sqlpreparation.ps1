b97b5eda938877b812ad63f98da2cca2645b217a
mauricio.cardona <mauricio.cardona@fisglobal.com>
2022-05-03 16:28:46 -0500

    Execution fix

--- sqlpreparation.ps1
+++ sqlpreparation.ps1
@@ -6,7 +6,8 @@ Param (
 (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/MauroTamariz/testing/main/sqlsettings.ps1','C:\sqlsettings.ps1')
 if ($ServiceAccount){
     (gc C:\sqlsettings.ps1) -replace 'accountToReplace', $ServiceAccount | Out-File -encoding ASCII C:\sqlsettings.ps1
+    cd C:\
+    ./sqlsettings.ps1
 }
-cd C:\
-./sqlsettings.ps1
+
 
